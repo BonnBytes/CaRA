053e439f7843f30627819dca60909daa3d7d7785
loki-veera <veeramacheneni.lokesh@proton.me>
2024-08-19 18:06:30 +0200

    Add cp init

--- train_val.py
+++ train_val.py
@@ -62,6 +62,7 @@ def _parse_args():
 def main():
     th.manual_seed(42)
     args = _parse_args()
+    print(args)
     name = "svhn"
     train_dl, test_dl = get_data(name)
     num_classes = get_classes_num(name)
@@ -94,21 +95,33 @@ def main():
         if "_ft" in name:
             param.requires_grad = True
 
-    init_fn = th.nn.init.xavier_normal_
+    # init_fn = th.nn.init.xavier_normal_
     # init_fn = th.nn.init.xavier_uniform_
     # init_fn = th.nn.init.eye_
     # init_fn = th.nn.init.orthogonal_
     # init_fn = th.nn.init.zeros_
-    # init_fn = "CP"
+    init_fn = "CP"
+    print("\n\nUsing CP init\n\n")
+
+    def split_weight(weight):
+        dim1, dim2 = weight.shape
+        if dim1 != 3 * dim2:
+            raise RuntimeError("dim1 != 3 * dim2")
+        dim1 //= 3
+        layer_weight = weight.reshape(3, dim1, dim2)
+        return layer_weight.unbind(0)
+
+
 
     for name, module in model.named_modules():
         if "qkv" in name and isinstance(module, CPLoraMerged):
             if init_fn == "CP":
                 decomp = tl.decomposition.CP(rank=args.trainable_ranks, normalize_factors=False, verbose=False, init="random", tol=1e-24, random_state=42)
-                Q_weight, K_weight, V_weight = _, _, _
-                _, (model_Q, heads_Q, headdim_Q) = decomp.fit_transform(Q_weight)
-                _, (model_K, heads_K, headdim_K) = decomp.fit_transform(K_weight)
-                _, (model_V, heads_V, headdim_V) = decomp.fit_transform(V_weight)
+                Q_weight, K_weight, V_weight = split_weight(module.weight.data)
+                tensor_fn = lambda x: x.reshape((x.shape[0], 12, x.shape[0]//12))
+                lambdas_Q, (model_Q, heads_Q, headdim_Q) = decomp.fit_transform(tensor_fn(Q_weight))
+                lambdas_K, (model_K, heads_K, headdim_K) = decomp.fit_transform(tensor_fn(K_weight))
+                lambdas_V, (model_V, heads_V, headdim_V) = decomp.fit_transform(tensor_fn(V_weight))
                 module.CPQ.S_model_ft = th.nn.Parameter(model_Q) 
                 module.CPK.S_model_ft = th.nn.Parameter(model_K) 
                 module.CPV.S_model_ft = th.nn.Parameter(model_V)
@@ -118,6 +131,9 @@ def main():
                 module.CPQ.S_headdim_ft = th.nn.Parameter(headdim_Q) 
                 module.CPK.S_headdim_ft = th.nn.Parameter(headdim_K) 
                 module.CPV.S_headdim_ft = th.nn.Parameter(headdim_V)
+                # module.CPQ.lambdas_ft = th.nn.Parameter(lambdas_Q)
+                # module.CPK.lambdas_ft = th.nn.Parameter(lambdas_K)
+                # module.CPV.lambdas_ft = th.nn.Parameter(lambdas_V)
 
 
             else:
@@ -140,11 +156,6 @@ def main():
             print(th.norm(module.CPK.S_model_ft), th.norm(module.CPK.S_heads_ft), th.norm(module.CPK.S_headdim_ft))
             print(th.norm(module.CPV.S_model_ft), th.norm(module.CPV.S_heads_ft), th.norm(module.CPV.S_headdim_ft))
             print()
-
-            
-    
-
-
 
     model.reset_classifier(num_classes)
     model = th.nn.DataParallel(model)
