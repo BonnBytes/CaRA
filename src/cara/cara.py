ff52c035cfa3695da2092176bf7bb8337d02cf73
loki-veera <veeramacheneni.lokesh@proton.me>
2025-07-28 14:31:59 +0200

    update flake8 and format

--- src/cara/cara.py
+++ src/cara/cara.py
@@ -1,25 +1,42 @@
-from copy import deepcopy
-from typing import Dict, Any
+"""Implement CaRA."""
 
+from typing import Any, Dict
+
+import tensorly as tl
+import timm
 import torch as th
 import torch.nn as nn
-import timm
-import tensorly as tl
+
 tl.set_backend("pytorch")
 
+global_model: th.nn.Module
 
-def cp_attn(self, x):
+
+def cp_attn(self, x: th.Tensor) -> th.Tensor:
+    """Attention with CP parameters.
+
+    Args:
+        x (th.Tensor): Input tensor.
+
+    Returns:
+        th.Tensor: CaRA attention output.
+    """
     B, N, C = x.shape
     qkv = self.qkv(x)
-    f1 = global_model.CP_A1[self.attn_idx:self.attn_idx+3, :]
-    tensor_attn = tl.cp_to_tensor((global_model.CP_R1, (f1, global_model.CP_A2, global_model.CP_A3, global_model.CP_A4)))
-    K, E, H, D = tensor_attn.shape
-    tensor_attn = tensor_attn.reshape((K, E, H*D))
-    qkv_delta = th.einsum("bnd, kde->kbne", x, self.dp(tensor_attn))
-    qkv_delta = qkv_delta.reshape(3, B, N, self.num_heads, C//self.num_heads).permute(
-        0, 1, 3, 2, 4
+    f1 = global_model.CP_A1[self.attn_idx : self.attn_idx + 3, :]
+    tensor_attn = tl.cp_to_tensor(
+        (
+            global_model.CP_R1,
+            (f1, global_model.CP_A2, global_model.CP_A3, global_model.CP_A4),
+        )
     )
-    qkv = qkv.reshape(B, N, 3, self.num_heads, C//self.num_heads).permute(
+    K, E, H, D = tensor_attn.shape
+    tensor_attn = tensor_attn.reshape((K, E, H * D))
+    qkv_delta = th.einsum("bnd, kde->kbne", x, self.dp(tensor_attn))
+    qkv_delta = qkv_delta.reshape(
+        3, B, N, self.num_heads, C // self.num_heads
+    ).permute(0, 1, 3, 2, 4)
+    qkv = qkv.reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(
         2, 0, 3, 1, 4
     )
     qkv += qkv_delta * self.s
@@ -28,56 +45,83 @@ def cp_attn(self, x):
     attn = attn.softmax(dim=-1)
     attn = self.attn_drop(attn)
 
-    x = (attn@v).transpose(1, 2).reshape(B, N, C)
+    x = (attn @ v).transpose(1, 2).reshape(B, N, C)
 
     proj = self.proj(x)
-    p1 = global_model.CP_P1[self.idx:self.idx+1, :]
-    tensor_proj = tl.cp_to_tensor((global_model.CP_R2, (p1, global_model.CP_P2, global_model.CP_P3)))
+    p1 = global_model.CP_P1[self.idx : self.idx + 1, :]
+    tensor_proj = tl.cp_to_tensor(
+        (global_model.CP_R2, (p1, global_model.CP_P2, global_model.CP_P3))
+    )
     AA, AB, AC = tensor_proj.shape
-    tensor_proj = tensor_proj.reshape((AA*AB, AC))
-    proj_delta = x@self.dp(tensor_proj.T) + global_model.CP_bias1
+    tensor_proj = tensor_proj.reshape((AA * AB, AC))
+    proj_delta = x @ self.dp(tensor_proj.T) + global_model.CP_bias1
     proj += proj_delta * self.s
     x = self.proj_drop(proj)
     return x
 
 
-def cp_mlp(self, x):
-    p1_up = global_model.CP_P1[self.idx:self.idx+4, :]
-    p1_down = global_model.CP_P1[self.idx+4: self.idx+8, :]
+def cp_mlp(self, x: th.Tensor) -> th.Tensor:
+    """Mlp with CP parameters.
+
+    Args:
+        x (th.Tensor): Input tensor.
+
+    Returns:
+        th.Tensor: Mlp projected output.
+    """
+    p1_up = global_model.CP_P1[self.idx : self.idx + 4, :]
+    p1_down = global_model.CP_P1[self.idx + 4 : self.idx + 8, :]
 
     up = self.fc1(x)
-    tensor_up = tl.cp_to_tensor((global_model.CP_R2, (p1_up, global_model.CP_P2, global_model.CP_P3)))
+    tensor_up = tl.cp_to_tensor(
+        (global_model.CP_R2, (p1_up, global_model.CP_P2, global_model.CP_P3))
+    )
     AA, AB, AC = tensor_up.shape
-    tensor_up = tensor_up.reshape((AA*AB, AC))
-    up_delta = x@self.dp(tensor_up.T) + global_model.CP_bias2
+    tensor_up = tensor_up.reshape((AA * AB, AC))
+    up_delta = x @ self.dp(tensor_up.T) + global_model.CP_bias2
     up += up_delta * self.s
 
     x = self.act(up)
     x = self.drop(x)
-    
+
     down = self.fc2(x)
-    tensor_down = tl.cp_to_tensor((global_model.CP_R2, (p1_down, global_model.CP_P2, global_model.CP_P3)))
-    tensor_down = tensor_down.reshape((AA*AB, AC))
-    down_delta = x@self.dp(tensor_down) + global_model.CP_bias3
+    tensor_down = tl.cp_to_tensor(
+        (global_model.CP_R2, (p1_down, global_model.CP_P2, global_model.CP_P3))
+    )
+    tensor_down = tensor_down.reshape((AA * AB, AC))
+    down_delta = x @ self.dp(tensor_down) + global_model.CP_bias3
     down += down_delta * self.s
     x = self.drop(down)
     return x
 
 
-def set_cara(model: nn.Module, rank: int, scale: float, l_mu: float, l_std: float):
-    if type(model) == timm.models.vision_transformer.VisionTransformer:
+def set_cara(
+    model: nn.Module, rank: int, scale: float, l_mu: float, l_std: float
+) -> None:
+    """Cara setup.
+
+    Args:
+        model (nn.Module): ViT model.
+        rank (int): FT Rank.
+        scale (float): FT scale.
+        l_mu (float): Init lambda_mu.
+        l_std (float): Init lambda_std.
+    """
+    if type(model) is timm.models.vision_transformer.VisionTransformer:
         # Declare CaRA parameters
         model.CP_A1 = nn.Parameter(th.empty([36, rank]), requires_grad=True)
         model.CP_A2 = nn.Parameter(th.empty([768, rank]), requires_grad=True)
         model.CP_A3 = nn.Parameter(th.empty([12, rank]), requires_grad=True)
-        model.CP_A4 = nn.Parameter(th.empty([768//12, rank]), requires_grad=True)
+        model.CP_A4 = nn.Parameter(
+            th.empty([768 // 12, rank]), requires_grad=True
+        )
         model.CP_P1 = nn.Parameter(th.empty([108, rank]), requires_grad=True)
         model.CP_P2 = nn.Parameter(th.empty([768, rank]), requires_grad=True)
         model.CP_P3 = nn.Parameter(th.empty([768, rank]), requires_grad=True)
         model.CP_R1 = nn.Parameter(th.empty([rank]), requires_grad=True)
         model.CP_R2 = nn.Parameter(th.empty([rank]), requires_grad=True)
         model.CP_bias1 = nn.Parameter(th.empty([768]), requires_grad=True)
-        model.CP_bias2 = nn.Parameter(th.empty([768*4]), requires_grad=True)
+        model.CP_bias2 = nn.Parameter(th.empty([768 * 4]), requires_grad=True)
         model.CP_bias3 = nn.Parameter(th.empty([768]), requires_grad=True)
         # Initialise CaRA parameters
         nn.init.xavier_normal_(model.CP_A1)
@@ -100,7 +144,7 @@ def set_cara(model: nn.Module, rank: int, scale: float, l_mu: float, l_std: floa
         model.idx = 0
         model.attn_idx = 0
     for child in model.children():
-        if type(child) == timm.models.vision_transformer.Attention:
+        if type(child) is timm.models.vision_transformer.Attention:
             child.dp = nn.Dropout(0.1)
             child.s = scale
             child.dim = rank
@@ -109,27 +153,35 @@ def set_cara(model: nn.Module, rank: int, scale: float, l_mu: float, l_std: floa
             global_model.idx += 1
             global_model.attn_idx += 3
             bound_method = cp_attn.__get__(child, child.__class__)
-            setattr(child, "forward", bound_method)
-        elif type(child) == timm.models.layers.mlp.Mlp:
+            setattr(child, "forward", bound_method)  # noqa: B010
+        elif type(child) is timm.models.layers.mlp.Mlp:
             child.dp = nn.Dropout(0.1)
             child.s = scale
             child.dim = rank
             child.idx = global_model.idx
             global_model.idx += 8
             bound_method = cp_mlp.__get__(child, child.__class__)
-            setattr(child, "forward", bound_method)
+            setattr(child, "forward", bound_method)  # noqa: B010
         elif len(list(child.children())) != 0:
             set_cara(child, rank, scale, l_mu, l_std)
-            
 
-def cara(config):
+
+def cara(config: Dict[str, Any]) -> th.nn.Module:
+    """Set CaRA for the given configuration.
+
+    Args:
+        config (Dict[str, Any]): Dictionary containing CaRA configuration.
+
+    Returns:
+        th.nn.Module: CaRA model.
+    """
     # CaRA parameters
     model = config["model"]
     rank = config["rank"]
     scale = config["scale"]
     l_mu = config["l_mu"]
     l_std = config["l_std"]
-    
+
     global global_model
     global_model = model
     set_cara(model, rank, scale, l_mu, l_std)
--- tests/test_cara.py
+++ tests/test_cara.py
@@ -1,19 +1,30 @@
-import torch as th
-import numpy as np
-import random
+"""Test CaRA module setup."""
 
-from src.cara.cara import cara
+import random
+from typing import Any, Dict
+
+import numpy as np
+import torch as th
 from timm.models import create_model
 
-
-def _get_vit():
-    return create_model(
-        "vit_base_patch16_224_in21k",
-        drop_path_rate=0.1
-        )
+from src.cara.cara import cara
 
 
-def _get_cara_config():
+def _get_vit() -> th.nn.Module:
+    """Create ViT model.
+
+    Returns:
+        th.nn.Module: ViT model.
+    """
+    return create_model("vit_base_patch16_224_in21k", drop_path_rate=0.1)
+
+
+def _get_cara_config() -> Dict[str, Any]:
+    """Create configuration dictionary.
+
+    Returns:
+        Dict[str, Any]: Configuration dictionary.
+    """
     random.seed(0)
     th.manual_seed(0)
     np.random.seed(0)
@@ -25,40 +36,63 @@ def _get_cara_config():
         "rank": 32,
         "scale": 1.0,
         "l_mu": 1.0,
-        "l_std": 0.0
-        }
+        "l_std": 0.0,
+    }
 
 
 def test_vit_without_cara():
+    """Vit test without CaRA."""
     vit = _get_vit()
-    assert (not hasattr(vit, "CP_A1")) and (not hasattr(vit, "CP_A2")) and (not hasattr(vit, "CP_A3")) and (not hasattr(vit, "CP_A4"))
-    assert (not hasattr(vit, "CP_P1")) and (not hasattr(vit, "CP_P2")) and (not hasattr(vit, "CP_P3"))
-    assert (not hasattr(vit, "CP_R1"))
-    assert (not hasattr(vit, "CP_R2"))
+    assert (
+        (not hasattr(vit, "CP_A1"))
+        and (not hasattr(vit, "CP_A2"))
+        and (not hasattr(vit, "CP_A3"))
+        and (not hasattr(vit, "CP_A4"))
+    )
+    assert (
+        (not hasattr(vit, "CP_P1"))
+        and (not hasattr(vit, "CP_P2"))
+        and (not hasattr(vit, "CP_P3"))
+    )
+    assert not hasattr(vit, "CP_R1")
+    assert not hasattr(vit, "CP_R2")
 
 
 def test_vit_with_cara():
+    """Vit test with CaRA module."""
     vit = cara(_get_cara_config())
-    assert (hasattr(vit, "CP_A1")) and (hasattr(vit, "CP_A2")) and (hasattr(vit, "CP_A3")) and (hasattr(vit, "CP_A4"))
-    assert (hasattr(vit, "CP_P1")) and (hasattr(vit, "CP_P2")) and (hasattr(vit, "CP_P3"))
-    assert (hasattr(vit, "CP_R1"))
-    assert (hasattr(vit, "CP_R2"))
+    assert (
+        (hasattr(vit, "CP_A1"))
+        and (hasattr(vit, "CP_A2"))
+        and (hasattr(vit, "CP_A3"))
+        and (hasattr(vit, "CP_A4"))
+    )
+    assert (
+        (hasattr(vit, "CP_P1"))
+        and (hasattr(vit, "CP_P2"))
+        and (hasattr(vit, "CP_P3"))
+    )
+    assert hasattr(vit, "CP_R1")
+    assert hasattr(vit, "CP_R2")
 
 
 def test_cara_zero_init():
+    """Check for zero initialisation in CaRA params."""
     vit = cara(_get_cara_config())
     assert th.allclose(vit.CP_A2, th.zeros_like(vit.CP_A2))
     assert th.allclose(vit.CP_P2, th.zeros_like(vit.CP_P2))
 
 
 def test_cara_lambda_init():
+    """Check for Lambda initis in CaRA params."""
     vit = cara(_get_cara_config())
     assert th.allclose(vit.CP_R1, th.ones_like(vit.CP_R1))
     assert th.allclose(vit.CP_R2, th.ones_like(vit.CP_R2))
 
 
 def test_cara_forward():
-    vit = _get_vit()
+    """Check for dummy forward pass."""
+    vit = cara(_get_cara_config())
     dummy_input = th.randn((2, 3, 224, 224))
     output = vit(dummy_input)
     assert np.allclose(list(output.shape), (2, 21843))
